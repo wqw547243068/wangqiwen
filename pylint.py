220b03c2f7ba3fde9d72bd46ba7459e3cc66e28c
wangqiwen <wqw547243068@163.com>
2017-11-24 19:37:38 +0800

    Update pylint.py

--- pylint.py
+++ pylint.py
@@ -13,13 +13,16 @@ class MyClass(object):
     public_name = '-public-' # public
     _myname = '-protected' # protected
     __private_name = '-private-' # private
+
     def __init__(self, name="wang"):
         self._myname = name
         print '我的名字是%s'%(self._myname)
+
     def say(self):
         """打招呼"""
         print '你好,我是%s,%s,%s'%(self._myname, self.public_name, self.__private_name)
         return 'yes'
+
     def modify(self, name="-"):
         """更改属性值"""
         self._myname = name
@@ -31,13 +34,19 @@ def my_fun(value=0, delta=9):
     res = value + delta
     return res
 
-if __name__ == '__main__':
+def main():
+    """main function"""
     #main里的都是全局变量,需要大写
-    A = 3
-    NEW = my_fun(A)
-    W = MyClass("wqw")
+    value = 3
+    new = my_fun(value)
+    v_result = MyClass("wqw")
     #不能访问protected、private变量.W._myname, W.__private_name
     #超过80字符时，可以用\换行，注：(),[]时可省略\
     print >> sys.stdout, 'hello,related values are listed as : %s , %s,I am \
-        %s,%s ...'%(A, NEW, W.say(), W.public_name)
+        %s,%s ...'%(value, new, v_result.say(), v_result.public_name)
+
+if __name__ == '__main__':
+    A = 3 # 此处为全局变量,一律大写
+    main()
+
 # */* vim: set expandtab ts=4 sw=4 sts=4 tw=400: */
