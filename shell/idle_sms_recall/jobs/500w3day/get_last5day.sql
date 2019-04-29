26211d8e5d6e8e3e7c52880768dd80210a2c02af
wangqiwen <wqw547243068@163.com>
2019-04-29 22:31:27 +0800

    del idle_sms

--- shell/idle_sms_recall/jobs/500w3day/get_last5day.sql
+++
@@ -1,3 +0,0 @@
-create or replace view wqw.last5day_users as 
-select distinct user_id from yay.dailyuseractivities where date_time >= DATE(date(nowcst()) - '5 day'::interval)
-
--- shell/idle_sms_recall/readme.sh
+++
@@ -1,6 +0,0 @@
-#更新文件到github服务器 
-git add *
-git commit -m "idle sms recall"
-git push
-#git push origin master
-
